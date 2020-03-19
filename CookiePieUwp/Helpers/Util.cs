20e495b47e325797eaa52ff01537d5a368c154ad
yuu-eguci <yuu.eguci@gmail.com>
2020-03-19 16:52:39 +0900

    Add sample util method

---
+++ CookiePieUwp/Helpers/Util.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CookiePieUwp.Helpers
+{
+    public static class Util
+    {
+        public static int AddOne(int n)
+        {
+            return n + 1;
+        }
+    }
+}
